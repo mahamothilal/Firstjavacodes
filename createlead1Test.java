80b135799138564214b2c5b9ef1a36ca49cf95ee
mahamothilal <88260013+mahamothilal@users.noreply.github.com>
2021-08-17 15:43:15 +0530

    createlead1Test.java

---
+++ createlead1Test.java
@@ -0,0 +1,21 @@
+package week2day1;
+
+import org.testng.annotations.Test;
+
+public class createlead1Test {
+
+  @Test
+  public void runcreateleadTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void rundeleteledTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void runeditleadTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
